52d44b2021d38d785fd2562cc89107859c76a5b1
Pavlo Medynskyy <medynskyypavlo@gmail.com>
2016-01-10 19:47:34 +0200

    insertion sort

---
+++ insertion_sort.js
@@ -0,0 +1,52 @@
+// метод который добавляет в обычный массив функцию смены позиций 2х элементов
+
+Array.prototype.swap = function(a,b) {
+	var cur = this[a];
+	this[a] = this[b];
+	this[b] = cur;
+}
+
+// функция для заполнения массива случайными числами
+
+Array.prototype.generate_numbers = function(amount){
+ for (var i = 0; i < amount; i++ ){
+ this[i] = Math.floor(Math.random() * amount + 1);
+ }
+}
+
+
+//метод срт-вки Кнута для массива
+function sorty(array){
+ var length = array.length;
+for (var i = 0; i < length; i++){
+for (var j = i; j > 0; j--){
+ if (array[j] < array[j-1] ){
+     array.swap(j, j-1);
+   } else {
+     break;
+   }
+  }
+}
+return array;
+}
+
+/*Array.prototype.knuth_shuffle = function() {
+  var random = 0;
+  for (var i = 1; i < this.length; i++){
+    var random = Math.floor(Math.random() * i);
+    this.swap(i, random); 
+  }
+};*/
+
+var res = [];
+res.generate_numbers(20);
+console.log(res);
+//res.knuth_shuffle();
+
+console.log(sorty(res));
+console.log('');
+//res = res.knuth_shuffle();
+//console.log(res);
+//res = res.knuth_shuffle();
+
+
